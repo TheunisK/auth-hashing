526ecb731c2d6124c925704574d1042d1e1a1658
Theunis Knoetze <theunis.knoetze@gmail.com>
2022-02-17 16:03:36 +0300

    complete but need to fix token creation for login

--- src/router.js
+++ src/router.js
@@ -2,15 +2,53 @@ const express = require('express');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const { PrismaClient } = require('@prisma/client');
+const { decodeBase64 } = require('bcryptjs');
 const prisma = new PrismaClient();
 
 const router = express.Router();
 
+const saltRounds = 10;
+const secret = "etsdkjnsdlkgmsls";
+
 router.post('/register', async (req, res) => {
-    
+    try {
+        const { username, password } = req.body;
+        const hash = await bcrypt.hash(password, saltRounds);
+        const user = { username: username, password: hash };
+        const newUser = await prisma.user.create({
+            data: user
+        });
+        res.json({newUser: user});
+
+    } catch(e) {
+        res.status(500).send("Broken!!!");
+        console.log(e);
+    }
+  
+
 });
 
 router.post('/login', async (req, res) => {
+    const { username, password  } = req.body;
+
+    const found = await prisma.user.findUnique({
+        where: {
+            username
+        }
+    });
+
+    if (found) {
+        if (bcrypt.compare(password, found.password)) {
+            const token = jwt.sign(username, secret);
+            return res.json({ newToken: token });
+        } else {
+            return res.json("Passwords dont match!");
+        }
+    } else {
+        return res.json("User does not exist.");
+    }
+
+
     
 });
 
